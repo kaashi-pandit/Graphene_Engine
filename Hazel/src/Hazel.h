b4a120dbbe1caea81d7e7160b2c7875e6f456f2e
piyushpandey24x7 <piyushphodu@gmail.com>
2024-06-12 21:24:24 +0800

    first working engine commit

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use by Hazel applications
+
+#include "Hazel/Application.h"
+
+//---------- ENTRY POINT [BEGIN] ----------
+
+#include "Hazel/EntryPoint.h"
+
+//---------- ENTRY POINT [END] ----------
